e162885ae51112aae599da9cb98ad82c652010e3
Razvan Anisia <anisia.razvan@gmail.com>
2019-05-29 21:41:10 +0100

    Updates

---
+++ precache-manifest.8d3e1b984e05aa732e20e7035e6cc3f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def46668b6c250fc9df5f665f4133b2b",
+    "url": "/air-quality-app/index.html"
+  },
+  {
+    "revision": "fb1a43186d82cdd0158e",
+    "url": "/air-quality-app/static/css/main.b3b69d41.chunk.css"
+  },
+  {
+    "revision": "ffbac6b16721fc77ff5d",
+    "url": "/air-quality-app/static/js/2.c8a69cbd.chunk.js"
+  },
+  {
+    "revision": "fb1a43186d82cdd0158e",
+    "url": "/air-quality-app/static/js/main.b2908633.chunk.js"
+  },
+  {
+    "revision": "f2c0e4ee7513addc28ae",
+    "url": "/air-quality-app/static/js/runtime~main.127a966c.js"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/air-quality-app/static/media/search-solid.c6a0effb.svg"
+  }
+]);
